bdf264d4ecfab406e01309e61c7cc75687f4f5c6
manikafsgarg1 <manikafsgarg@gmail.com>
2018-05-12 11:57:52 +0530

    Signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
   get  '/contact', to: 'static_pages#contact'
   get  '/about', to: 'static_pages#about'
   get  '/help', to: 'static_pages#help'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  '/signup',  to: 'users#new'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
